45ed01b10accbf8175457ddfaee4f8e3e129a734
Ercin Dedeoglu <e.dedeoglu@gmail.com>
2024-06-21 11:24:59 +0400

    🔧 (Dockerfile): update entrypoint script description to include workspace permissions 🔧 (entrypoint.sh): add commands to set correct permissions for Jenkins workspace directory

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,5 +6,9 @@ sudo usermod -aG docker jenkins
 # Change the permissions of the Docker socket
 sudo chmod 666 /var/run/docker.sock
 
+# Correct permissions for the Jenkins workspace directory
+sudo chown -R jenkins:jenkins /home/jenkins/agent/workspace
+sudo chmod -R 775 /home/jenkins/agent/workspace
+
 # Execute the original entrypoint command
 exec /usr/local/bin/jenkins-agent "$@"
