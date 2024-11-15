b63fa052e342cbb3db4ad9241ec185370936ceac
ErcinDedeoglu <e.dedeoglu@gmail.com>
2024-11-15 10:19:25 +0400

    build(Dockerfile): streamline Docker installation and improve permissions handling
    Simplify Docker installation by combining steps and removing redundant commands. Add essential packages for network and module management. Ensure necessary kernel modules are loaded for Docker functionality.
    
    fix(entrypoint.sh): ensure Docker daemon starts and handle user permissions
    
    Add logic to start Docker daemon as root and wait for it to be ready. Improve user permission handling and ensure Jenkins user can access Docker functionalities.

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-# Ensure the Jenkins user is added to the docker group
-sudo usermod -aG docker jenkins
+# Start the Docker daemon as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Switching to root to start Docker daemon"
+  exec sudo -E "$0" "$@"
+fi
 
-# Change the permissions of the Docker socket
-sudo chmod 666 /var/run/docker.sock
+# Attempt to modify user permissions before switching users
+if ! usermod -aG docker jenkins; then
+  echo "Warning: Failed to add Jenkins to the docker group. Permissions might be limited."
+fi
 
-# Correct permissions for the Jenkins workspace directory
-sudo chown -R jenkins:jenkins /home/jenkins/agent/workspace
-sudo chmod -R 775 /home/jenkins/agent/workspace
+# Start the Docker daemon with vfs as the storage driver
+dockerd --storage-driver=vfs &
 
-# Execute the original entrypoint command
-exec /usr/local/bin/jenkins-agent "$@"
+# Wait for the Docker daemon to start
+while (! docker info > /dev/null 2>&1 ); do
+  echo "Waiting for Docker daemon to start..."
+  sleep 1
+done
+
+# Switch to Jenkins user and run Jenkins agent
+sudo -u jenkins -E /bin/bash -c '
+  mkdir -p /home/jenkins/agent/workspace
+  chown -R jenkins:jenkins /home/jenkins/agent/workspace
+  chmod -R 775 /home/jenkins/agent/workspace
+
+  exec /usr/local/bin/jenkins-agent "$@"
+'
